40f4b8ec4b2172dffd184c66e535d5918adff5ec
Vasyl.Burlaka <vasiliy.burlaka@gmail.com>
2021-01-12 03:22:14 +0200

    instantiate enemies in loop

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaveConfig currentWave = waveConfigs[startWave];
+        StartCoroutine(SpawnAllEnemies(currentWave));
+    }
+
+    IEnumerator SpawnAllEnemies(WaveConfig waveConfig)
+    {
+        Debug.Log("SpawnAllEnemies");
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+            Debug.Log("SpawnAllEnemies LOOP " + i.ToString());
+            Instantiate(
+                waveConfig.GetEnemyPrefab(), 
+                waveConfig.GetWaypoints()[0].transform.position, 
+                Quaternion.identity
+            );
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
